c0720fdeb8496a876a9c21af91b89f0864a8ef34
Rick Hornsby <github@w.ktzr.net>
2017-08-18 21:25:04 -0500

    Filter user input

--- manager.rb
+++ manager.rb
@@ -4,6 +4,10 @@ require 'sinatra/cross_origin'
 require 'sinatra/json'
 require 'pp'
 
+MAX_PHRASE_LENGTH = 128
+MAX_PHRASE_DURATION = 16
+MIN_PHRASE_DURATION = 2
+
 class MirrorManager < Sinatra::Application
 
   before do
@@ -23,18 +27,18 @@ class MirrorManager < Sinatra::Application
 
   post '/phrases' do
     now = Time.new
-    json_data = Hash.new
-    json_data['last_modified'] = now.to_s
+    json_data = { 'last_modified': now.to_s }
+
     payload = params
     payload = JSON.parse(request.body.read) unless params[:path]
-    json_data['phrases'] = payload['phrases']
+    json_data['phrases'] = filter_phrase_data(payload['phrases'])
+
     json_text = json_data.to_json
-    # TODO: validate the input...
     File.write('phrases.json', json_text)
     'OK'.to_json
   end
 
-  error 401 do
+  error 400..410 do
     'Error 401 - Authentication required'
   end
 
@@ -45,4 +49,22 @@ class MirrorManager < Sinatra::Application
     response.headers['Access-Control-Allow-Origin'] = '*'
     200
   end
+
+  def filter_phrase_data(phrase_data)
+    filtered_data = []
+    phrase_data.each do |phrase|
+      next if phrase['text'].length == 0
+
+      duration = phrase['duration'].to_i
+      duration = MIN_PHRASE_DURATION if duration < MIN_PHRASE_DURATION
+      duration = MAX_PHRASE_DURATION if duration > MAX_PHRASE_DURATION
+      phrase['text'] = phrase['text'][0..MAX_PHRASE_LENGTH] if phrase['text'].length > MAX_PHRASE_LENGTH
+
+      filtered_data << { text: phrase['text'], duration: duration }
+    end
+
+    filtered_data
+
+  end
+
 end
